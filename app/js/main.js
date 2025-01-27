c2b59f6e9b57b1b4c9cbe8d9bb61b75d170dd114
DmitryRudinsky <guladmitriy1331@gmail.com>
2025-01-27 03:49:11 +0300

    mainpage done

---
+++ app/js/main.js
@@ -0,0 +1,41 @@
+import {isValidForm} from './utils.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const animatedTitle = document.getElementById('animatedTitle');
+    const animatedSubTitle = document.getElementById('animatedSubTitle');
+    const animatedForm = document.getElementById('animatedForm');
+    setTimeout(() => {
+        animatedTitle.classList.add('visible');
+    }, 1000);
+    setTimeout(() => {
+        animatedSubTitle.classList.add('visible');
+        animatedForm.classList.add('visible');
+    }, 1500);
+});
+
+document.getElementById('animatedForm').addEventListener('submit', (event) => {
+    event.preventDefault();
+    const emailError = document.getElementById('emailError');
+    const formData = new FormData(event.target);
+    const formValues = Object.fromEntries(formData.entries());
+    const [nonField, isMailValid] = isValidForm(formValues);
+
+    if (!isMailValid) {
+        emailError.style.display = 'block';
+    } else {
+        emailError.style.display = 'none';
+    }
+
+    if (!nonField.length && isMailValid) {
+        Object.entries(formValues).forEach(([key, value]) => {
+            const field = document.getElementById(key);
+            field.classList.remove('emptyField')
+            localStorage.setItem(key, value);
+        });
+    } else {
+        nonField.forEach((event) => {
+            const field = document.getElementById(event);
+            field.classList.add('emptyField');
+        })
+    }
+});
